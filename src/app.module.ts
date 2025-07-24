d4dc7fc42dadd5aa2cd6bce09af63dc763bc5d3c
hymer007 <dieterschlack@gmail.com>
2025-07-24 13:52:18 +0200

    enu

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller'; // hinzufügen ✅
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [AppController], // hinzufügen ✅
 })
 export class AppModule {}
